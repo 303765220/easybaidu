c96d1f04e20f2b813b06fa96b17c17d33cbff47f
禁锢之翼 <mednovahw@163.com>
2018-04-04 15:27:14 +0800

    新增身份证识别

--- src/baidu.php
+++ src/baidu.php
@@ -22,14 +22,63 @@ class baidu
 
     public function __construct()
     {
-        $this->check();
+        $this->checkParame();
         $this->client = new \AipOcr($this->appid, $this->appkey, $this->secretkey);
     }
+
+
+    /**
+     *
+     * 识别营业执照
+     * @param $image 图片资源
+     * @return mixed
+     * @throws BaiduException
+     */
+    public function license($image){
+        $result = $this->client->businessLicense($image);
+        return $this->formatResult($result);
+    }
+
+    /**
+     * 识别身份证照片
+     * @param $image 图片资源
+     * @param string $detect_direction 是否检测图像朝向  true 和 false
+     * @param string $detect_risk 是否开启身份证风险类型  true 和 false
+     * @return mixed
+     * @throws BaiduException
+     */
+    public function idcard($image,$detect_direction="true",$detect_risk="false")
+    {
+        $options = array();
+        $options["detect_direction"] = $detect_direction;
+        $options["detect_risk"] =  $detect_risk;
+
+        $result = $this->client->idcard($image,'front',$options);
+        return $this->formatResult($result);
+    }
+
+
+    /**
+     * 识别银行卡照片
+     * @param $image  图片资源
+     * @return mixed
+     * @throws BaiduException
+     */
+    public function bankcard($image)
+    {
+        $result = $this->client->bankcard($image);
+        if(array_key_exists('error_code',$result))
+        {
+            throw new BaiduException("错误码：".$result['error_code']);
+        }
+        return $result['result'];
+    }
+
 
     /**
      * 验证必要参数
      */
-    private function check()
+    private function checkParame()
     {
         if(is_null(config('baidu'))){
             throw new BaiduException('请先执行php artisan vendor:publish --provider="Xiaoyi\EasyBaidu\Providers\BaiduServiceProvider"');
@@ -52,45 +101,11 @@ class baidu
         }
     }
 
-
     /**
-     *
-     * 识别营业执照
-     * @param $image 图片资源
-     * @return mixed
-     * @throws BaiduException
+     * 格式化返回数据
      */
-    public function license($image){
-        $result = $this->client->businessLicense($image);
-
-        if(array_key_exists('error_code',$result))
-        {
-            throw new BaiduException("错误码：".$result['error_code']);
-        }
-
-        foreach ($result['words_result'] as $k => $v)
-        {
-            $reset[$k] = $v['words'];
-        }
-        return $reset;
-    }
-
-    /**
-     * 识别身份证照片
-     * @param $image 图片资源
-     * @param string $detect_direction 是否检测图像朝向  true 和 false
-     * @param string $detect_risk 是否开启身份证风险类型  true 和 false
-     * @return mixed
-     * @throws BaiduException
-     */
-    public function idcard($image,$detect_direction="true",$detect_risk="false")
+    private function formatResult($result)
     {
-        $options = array();
-        $options["detect_direction"] = $detect_direction;
-        $options["detect_risk"] =  $detect_risk;
-
-        $result = $this->client->idcard($image,'front',$options);
-
         if(array_key_exists('error_code',$result))
         {
             throw new BaiduException("错误码：".$result['error_code']);
@@ -103,6 +118,5 @@ class baidu
 
         return $reset;
     }
-
 
 }
